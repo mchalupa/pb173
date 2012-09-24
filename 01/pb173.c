69a3b2662c8483dbb66b0da016e5abf9c7419374
marek <marek@debian.(none)>
2012-09-24 08:11:54 -0400

    01 module dev version done

--- 01/pb173.c
+++ 01/pb173.c
@@ -2,6 +2,23 @@
 
 static int my_init(void)
 {
+	printk(KERN_INFO "Hello world!\n");
+	
+	void *mem = kmalloc(1000, GFP_KERNEL);
+	if (mem) {
+		printk(KERN_INFO "Got memory\n");
+		strcpy(mem, "Bye");
+		printk("mem: %p\n", mem);
+		printk("stack: %p\n", &mem);
+		printk("str: %s\n", (char *)  mem);
+		printk("jiffies: %p\n", &jiffies);
+		printk("myfunc: %p\n", &my_init);
+		printk("otherfunc: %p\n", &printk);
+		printk("%x\n", __builtin_return_address(0));
+	}
+
+	kfree(mem);
+
 	return 0;
 }
 
