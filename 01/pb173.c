f08c2bcd7f4c219ebc3ac2ca494bffb512261282
root <root@debian.(none)>
2012-09-26 13:15:57 -0400

    Homework done

--- 01/pb173.c
+++ 01/pb173.c
@@ -17,6 +17,29 @@ static void my_exit(void)
 	if (mem) {
 		strcpy(mem, "Bye");
 		printk(KERN_INFO "%s\n", (char *)  mem);
+
+		/*
+		 * HOMEWORK
+		 */
+
+		/* print adress of allocated variable */
+		printk(KERN_INFO "%pS\n", mem);
+
+		/* print adress of variable on the stack */
+		printk(KERN_INFO "%p\n", &mem);
+
+		/* print adress of jiffies */
+		printk(KERN_INFO "%p\n", &jiffies);
+
+		/* print adress of some function in this module */
+		printk(KERN_INFO "%p", &my_init);
+
+		/* print adress of some func outside of this module */
+		printk(KERN_INFO "%p\n", &vprintk);
+
+		/* print name + offset of return adress of this function*/
+		printk(KERN_INFO "%pF\n", __builtin_return_address(0));
+
 		kfree(mem);
 	}
 }
