84ae87f3417d98cc6bd60961c01120cffb29fa32
Marek Chalupa <mchalupa@mail.muni.cz>
2012-11-19 15:29:38 -0500

    COMBO driver and PCI devices list.
    Homework 8. COMBO driver with decoding of information area and
    list of PCI devices created while initializing module and
    compared to actual PCI devices when exiting module. New devices
    are to be prited to log.

--- 07/pb173.c
+++ 07/pb173.c
@@ -2,8 +2,19 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/mm.h>
+#include <linux/list.h>
+
+struct mdata {
+	struct list_head list;
+	int domain;
+	int bus;
+	int slot;
+	int func;
+};
 
 static void* iomem;
+
+LIST_HEAD(l);
 
 static int mprobe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
@@ -11,7 +22,8 @@ static int mprobe(struct pci_dev *pdev, const struct pci_device_id *id)
 	printk("%.2x:%.2x:%.2x\n", pdev->bus->number, PCI_SLOT(pdev->devfn), PCI_FUNC(pdev->devfn));
 	printk("vendor %.2x, device %.2x\n", pdev->vendor, pdev->device);
 
-	pci_enable_device(pdev);
+	if (! pci_enable_device(pdev))
+		return -EBUSY;
 
 	if (pci_request_region(pdev, 0, "Only MAAAINNN region") != 0) {
 		pci_disable_device(pdev);
@@ -46,28 +58,55 @@ static struct pci_driver driver = {
 	.id_table = table,
 };
 
-
 static int my_init(void)
 {
-
-	__u32 dword;
-
-	/*
+	__u32 dword;	/* for encoding COMBO info area */
 	struct pci_dev *pdev = NULL;
-	while((pdev = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, pdev)))
-		pci_dev_get(..)  -- zvyseni reference
-		nezxapomenout v release zavolat pci_dev_put()
-		printk("[bus:vendor:dev] %.2x:%.2x:%.2x\n", 
+	struct mdata *tmp;
+	struct list_head *pos, *n;
+
+	while((pdev = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, pdev))) {
+		/*
+		printk(KERN_DEBUG "[bus:vendor:dev] %.2x:%.2x:%.2x\n",
 				pdev->bus->number, pdev->vendor, pdev->device);
-	*/
+		*/
 
-	pci_register_driver(&driver);
+		pci_dev_get(pdev);
 
+		tmp = kmalloc(sizeof(struct mdata), GFP_KERNEL);
+
+		if (! tmp) {
+			/* free already allocated memory upon error*/
+			list_for_each_safe(pos, n, &l) {
+				tmp = list_entry(pos, struct mdata, list);
+				list_del(pos);
+				kfree(tmp);
+			}
+			return -ENOMEM;
+		} else {
+			tmp->domain = pci_domain_nr(pdev->bus);
+			tmp->bus = pdev->bus->number;
+			tmp->slot = PCI_SLOT(pdev->devfn);
+			tmp->func = PCI_FUNC(pdev->devfn);
+
+			list_add(&(tmp->list), &l);
+		}
+	}
+
+	/********************************/
+	/* COMBO part			*/
+	/********************************/
+
+	/* register COMBO driver */
+	if (! pci_register_driver(&driver))
+		return -EBUSY;
+
+	/* revisions */
 	dword = readl(iomem);
 	printk("major rev: %x, minor rev: %x\n", (dword  >> 8) & 0xff, dword & 0xff);
 
+	/* date */
 	dword = readl(iomem + 4);
-
 	printk("20%02d-%02d-%02d %d:%d\n", dword >> 7*4, (dword  >> 6*4) & 0xf,
 					(dword  >> 4*4) & 0xff, (dword  >> 2*4) & 0xff, (dword & 0xff));
 
@@ -76,6 +115,34 @@ static int my_init(void)
 
 static void my_exit(void)
 {
+	struct mdata *tmp;
+	struct list_head *pos, *n;
+	struct pci_dev *pdev = NULL;
+	int present = 0; /* bool for comparing list of devices */
+
+	while((pdev = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, pdev))) {
+		list_for_each_entry(tmp, &l, list) {
+			if (tmp->domain == pci_domain_nr(pdev->bus) &&
+				tmp->bus == pdev->bus->number &&
+				tmp->slot == PCI_SLOT(pdev->devfn) &&
+				tmp->func == PCI_FUNC(pdev->devfn))
+				present = 1;
+		}
+		if (! present)
+			/* print new devices */
+			printk(KERN_INFO "%.2x:%.2x\n", pdev->vendor, pdev->device);
+	}
+
+	list_for_each_safe(pos, n, &l) {
+		tmp = list_entry(pos, struct mdata, list);
+
+		pci_dev_put(pci_get_bus_and_slot(tmp->bus, PCI_DEVFN(tmp->slot, tmp->func)));
+
+		list_del(pos);
+		kfree(tmp);
+	}
+
+	/* unregister COMBO driver */
 	pci_unregister_driver(&driver);
 }
 
