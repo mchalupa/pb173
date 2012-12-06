9f136c421f0a9a3f5ddd37186cf863f3895a84da
root <root@satyr09.fi.muni.cz>
2012-12-06 14:29:34 +0100

    DMA implemented

--- 07/pb173.c
+++ 07/pb173.c
@@ -5,6 +5,8 @@
 #include <linux/list.h>
 #include <linux/interrupt.h>
 #include <linux/timer.h>
+#include <linux/dma-mapping.h>
+#include <linux/delay.h>
 
 #define BYTE(ptr, n) ((*((__u8 *) ptr) + n) & 0xff)
 
@@ -22,6 +24,9 @@ struct mdata {
 
 static void* iomem;
 
+dma_addr_t dma_phys;
+void *dma_virt;
+
 LIST_HEAD(l);
 
 static irqreturn_t irqhandler(int irq, void *data, struct pt_regs *ptregs)
@@ -37,6 +42,16 @@ static irqreturn_t irqhandler(int irq, void *data, struct pt_regs *ptregs)
 	}
 }
 
+static void run_dma(__u32 from, __u32 to, __u8 bus1, __u8 bus2, __u32 size, __u8 interrupt)
+{
+	/* set DMA registry */
+	writel(from, iomem + 0x80);
+	writel(to, iomem + 0x84);
+	writel(size, iomem + 0x88);
+
+	writel((1 | bus1 << 1 | bus2 << 4 | interrupt << 7), iomem + 0x8c);
+}
+
 static int mprobe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
 	__u32 dword;	/* for decoding COMBO info area */
@@ -44,11 +59,14 @@ static int mprobe(struct pci_dev *pdev, const struct pci_device_id *id)
 
 	printk("Probing mpcidriver...\n");
 	printk("%.2x:%.2x:%.2x\n", pdev->bus->number, PCI_SLOT(pdev->devfn),
-					PCI_FUNC(pdev->devfn));
+				PCI_FUNC(pdev->devfn));
 	printk("vendor %.2x, device %.2x\n", pdev->vendor, pdev->device);
 
 	if (pci_enable_device(pdev) < 0)
 		return -EBUSY;
+
+	pci_set_dma_mask(pdev, DMA_BIT_MASK(32));
+	pci_set_master(pdev);
 
 	if (pci_request_region(pdev, 0, "Only MAAAINNN region") < 0) {
 		pci_disable_device(pdev);
@@ -80,15 +98,43 @@ static int mprobe(struct pci_dev *pdev, const struct pci_device_id *id)
 	/* revisions */
 	dword = readl(iomem);
 	printk("major rev: %x, minor rev: %x\n", BYTE(&dword, 1),
-							BYTE(&dword, 0));
+						BYTE(&dword, 0));
 
 	/* date */
 	dword = readl(iomem + 4);
 	/* 0xYMDDhhmm */
 	printk("20%02d-%02d-%02d %d:%d\n", BYTE(&dword, 3) >> 4,
-						BYTE(&dword, 3) & 0xf,
-						BYTE(&dword, 2), BYTE(&dword, 1),
-						BYTE(&dword, 0));
+					BYTE(&dword, 3) & 0xf,
+					BYTE(&dword, 2), BYTE(&dword, 1),
+					BYTE(&dword, 0));
+
+	
+	dma_virt = dma_alloc_coherent(NULL, PAGE_SIZE, &dma_phys, GFP_KERNEL);
+
+	if (! dma_virt) {
+		iounmap(iomem);
+		pci_release_region(pdev, 0);
+		pci_disable_device(pdev);
+		return -ENOMEM;
+	}
+
+	strcpy(dma_virt, "1234567890");
+
+	run_dma(dma_phys, 0x40000, 2, 4, 10, 1);
+
+	while(readl(iomem + 0x8c) & 0x1)
+		msleep(100);
+
+	printk(KERN_INFO "DMA transfer (-> ward)  completed\n");
+
+	run_dma(0x40000, dma_phys + 10, 4, 2, 10, 1);
+
+	while(readl(iomem + 0x8c) & 0x1)
+		msleep(100);
+
+	printk(KERN_INFO "DMA transfer (<- ward)  completed\n");
+	printk(KERN_INFO "mem: %20s\n", (char *) dma_virt);
+
 
 	return 0;
 }
@@ -97,7 +143,9 @@ static void mremove(struct pci_dev *pdev)
 {
 	printk("Removing mpcidriver\n");
 	printk("%.2x:%.2x:%.2x\n", pdev->bus->number, PCI_SLOT(pdev->devfn),
-							PCI_FUNC(pdev->devfn));
+						PCI_FUNC(pdev->devfn));
+
+	dma_free_coherent(NULL, PAGE_SIZE, dma_virt, dma_phys);
 
 	free_irq(pdev->irq, iomem);
 
@@ -127,7 +175,7 @@ static void raise_interrupt(unsigned long data)
 	/* raise interrupt */
 	writel(COMBO_INT_NO, iomem + RAISE_INT);
 
-	mod_timer(&int_timer, jiffies + msecs_to_jiffies(100));
+/*	mod_timer(&int_timer, jiffies + msecs_to_jiffies(100));*/
 }
 
 
@@ -136,6 +184,7 @@ static int my_init(void)
 	struct pci_dev *pdev = NULL;
 	struct mdata *tmp;
 	struct list_head *pos, *n;
+
 
 	while((pdev = pci_get_device(PCI_ANY_ID, PCI_ANY_ID, pdev))) {
 		tmp = kmalloc(sizeof(struct mdata), GFP_KERNEL);
